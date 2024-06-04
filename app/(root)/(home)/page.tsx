cfb2215c81404cc293f621782e0cde47a223f946
Gabriela França <gabifmouraroc@outlook.com>
2024-06-03 22:14:57 -0300

    questions homePage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,7 @@
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
-import NoResult from "@/components/shared/NoResult";
+import NoResult from "@/components/shared/NoResults";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
@@ -23,7 +23,7 @@ const questions = [
     upvotes: 1500000,
     views: 500552,
     answers: [],
-    createdAt: new Date("2023-09-01T12:00:00.000Z"),
+    createdAt: new Date("2024-06-02T12:00:00.000Z"),
   },
   {
     _id: "2",
@@ -40,7 +40,7 @@ const questions = [
     upvotes: 5,
     views: 50,
     answers: [],
-    createdAt: new Date("2021-09-02T10:30:00.000Z"),
+    createdAt: new Date("2024-05-02T10:30:00.000Z"),
   },
 ];
 
@@ -74,6 +74,31 @@ const Home = () => {
       </div>
 
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
